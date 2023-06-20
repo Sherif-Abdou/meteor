70a0f47cf3bf9ad6ad3d6cd43cd974e403119a3a
Sherif Abdou <sherif_abdou@outlook.com>
2023-06-20 14:41:22 -0400

    triangle exists

--- main.cpp
+++ main.cpp
@@ -1,8 +1,85 @@
 #include <iostream>
+using std::string;
 
-//#include <glad/gl.h>
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <fstream>
+#include <sstream>
+
+string readFile(const char* path) {
+    std::ifstream input_stream;
+    std::stringstream strstream;
+    input_stream.open(path);
+
+    strstream << input_stream.rdbuf();
+    string text = strstream.str();
+
+    input_stream.close();
+    return text;
+}
+
+unsigned int createShaderProgram(const char* vPath, const char* fPath) {
+    unsigned int vertex_shader;
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    string vFile = readFile(vPath);
+    const char* vFileC = vFile.c_str();
+    glShaderSource(vertex_shader, 1, &vFileC, nullptr);
+
+    unsigned int fragment_shader;
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    string fFile = readFile(fPath);
+    const char* fFileC = fFile.c_str();
+    glShaderSource(fragment_shader, 1, &fFileC, nullptr);
+
+    glCompileShader(vertex_shader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << vFile << "\n";
+    }
+
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << vFile << "\n";
+    }
+
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    return shader_program;
+}
+
+unsigned int generateTriangleVAO() {
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    float vertices[] {0.8f, 0.8f, 0.0f,
+                        0.0f, 0.0f, 0.0f,
+                        0.0f, 0.8f, 0.0f};
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9*sizeof(float), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, (void*)0);
+    glEnableVertexAttribArray(0);
+
+    return vao;
+}
 
 int main() {
     if (!glfwInit())
@@ -13,9 +90,21 @@ int main() {
     GLFWwindow* window = glfwCreateWindow(800, 600, "Meteor", nullptr, nullptr);
 
     glfwMakeContextCurrent(window);
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (!version) {
+        glfwTerminate();
+        return -1;
+    }
+
+    auto shader_program = createShaderProgram("shaders/vertex.vert", "shaders/fragment.frag");
+    auto vao = generateTriangleVAO();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shader_program);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
---
+++ shaders/fragment.frag
@@ -0,0 +1,6 @@
+#version 410 core
+out vec4 color;
+
+void main() {
+    color = vec4(0.3f, 0.4f, 0.5f, 1.0f);
+}
